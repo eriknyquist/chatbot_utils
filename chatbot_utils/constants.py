f35a90f99a699d4eee3ab5589ddf756331387a65
Erik Nyquist <eknyquist@gmail.com>
2022-05-07 17:59:05 -0700

    Add missing py file

---
+++ chatbot_utils/constants.py
@@ -0,0 +1,8 @@
+# Token that separates the variable assignment section from the rest of the response text
+VAR_ASSIGNMENT_SECTION_SEP = ";;"
+
+# Token that separates individual assignments within the variable assignments section
+VAR_ASSIGNMENT_SEP = ","
+
+# Token that separates the two sides within an individual assignment
+VAR_ASSIGNMENT_OP = "="
